1addc9db5197d1fdffcf6e4123acf3e1756bac91
semarslan <sema.arslan@bil.omu.edu.tr>
2021-01-17 18:16:53 +0300

    .

---
+++ parent-service/src/main/java/com/semarslan/parentservice/ParentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.semarslan.parentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentServiceApplication.class, args);
+    }
+
+}
